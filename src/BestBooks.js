fb4b5ac1023e289ef0577edd04723485102fc9f3
Katharine Swilley <kswilley@gmail.com>
2022-05-23 17:54:36 -0700

    working on front end tasks

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+import books from ''
+
+let SERVER = process.env.REACT_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,6 +13,21 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getBooks = async() => {
+    try {
+      let results = await axios.get(`${SERVER}/books`);
+      this.setState = {
+        books: results
+      }
+      console.log(books);
+    } catch(error) {
+      console.log('There has been an error');
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
 
   render() {
 
